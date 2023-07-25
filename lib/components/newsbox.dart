c096e55120a9244bb13bde5558324abc4a043d74
Nishan Bishwokarma <nishanbishokarma98@gmail.com>
2023-07-25 22:17:59 +0545

    News added successfully

--- lib/components/newsbox.dart
+++ lib/components/newsbox.dart
@@ -1,4 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:newsapp/utils/colors.dart';
+import 'package:newsapp/utils/text.dart';
 
 class NewsBox extends StatelessWidget {
   final String imageurl, title, time, description, url;
@@ -14,6 +17,44 @@ class NewsBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        InkWell(
+          onTap: () {},
+          child: Row(children: [
+            CachedNetworkImage(
+              imageUrl: imageurl,
+              imageBuilder: (context, imageProvider) => Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.yellow,
+                ),
+              ),
+              placeholder: (context, url) => CircularProgressIndicator(
+                color: AppColors.primary,
+              ),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
+            SizedBox(width: 8.0),
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomText(text: title, size: 16, color: AppColors.white),
+                CustomText(text: time, size: 16, color: AppColors.lightwhite),
+              ],
+            ))
+          ]),
+        ),
+        // DividerWidget(),
+      ],
+    );
   }
 }
